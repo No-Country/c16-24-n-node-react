153098cf3df8b2e87276130e5b756177188f8826
lucacarena97 <lucacarena98@gmail.com>
2024-02-13 19:58:03 -0300

    add directory login

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
-import { Login } from "./routes/Login";
+import { Login } from "./routes/Login/Login";
 import NavBar from "./components/Navbar";
+import { Home } from "./routes/Home/Home";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </div>
   );
