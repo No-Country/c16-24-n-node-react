08afa3fd3c4f5c010efefe5e0e935d94068908cb
andavian <ing.elbaum@gmail.com>
2024-02-14 00:54:48 -0300

    creacion de modelos y conexion BBDD

--- api/index.js
+++ api/index.js
@@ -1,20 +1,11 @@
-const express = require("express");
-const morgan = require("morgan");
 require("dotenv").config();
-const server = express();
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+const { PORT } = process.env;
 
-// Configuración de Morgan para el registro de solicitudes HTTP
-server.use(morgan("dev"));
-
-// Ruta de ejemplo
-server.get("/", (req, res) => {
-  res.send("¡Hola mundo desde Express!");
-});
-
-// Configuración del puerto
-const PORT = process.env.PORT || 3000;
-
-// Iniciar el servidor
-server.listen(PORT, () => {
-  console.log(`Servidor Express corriendo en http://localhost:${PORT}`);
+// Syncing all the models at once.
+conn.sync({ alter: false }).then(() => {
+  server.listen(PORT, () => {
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
+  });
 });
---
+++ api/src/models/User.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require("sequelize");
+module.exports = (sequelize) => {
+  sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      user_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      banned: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+    },
+    { freezeTableName: true, timestamps: true }
+  );
+};
