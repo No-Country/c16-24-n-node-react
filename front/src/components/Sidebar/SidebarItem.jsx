998d7bf9eff41531039e3d8b78118f733dd58a33
Jorge Trujillo <jorge.trujillo@epn.edu.ec>
2024-03-03 18:30:56 -0500

    Sidebar to movile devices

--- front/src/components/Sidebar/SidebarItem.jsx
+++ front/src/components/Sidebar/SidebarItem.jsx
@@ -22,14 +22,14 @@ const SidebarItem = ({expanded}) => {
   return (
     <>
       {items.map((item) => (
-        <li key={item.id} className="py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group hover:bg-indigo-50 text-gray-600">
+        <li key={item.id} className="p-0 md:py-2 md:px-3 md:my-1 font-medium rounded-md cursor-pointer transition-colors group hover:bg-indigo-50 text-gray-600 relative">
           <NavLink to={item.to} className="relative flex items-center text-gray-800 hover:text-blue-400 duration-500" >
-            <span className="">{item.icon} </span>
-            <span className={`isMobile text-xl overflow-hidden transition-all ${ expanded ? "w-52 ml-3" : "w-0"}`}>
+            <span>{item.icon} </span>
+            <span className={`text-xl overflow-hidden transition-all ${ expanded ? "md:w-52 md:ml-3" : "w-0 h-0"}`}>
               {item.text}
             </span>
             {!expanded && (
-              <div className={`absolute left-full rounded-md px-2 py-1 ml-6 bg-indigo-100 text-indigo-800 text-sm invisible opacity-20 -translate-x-3 transition-all group-hover:visible group-hover:opacity-100 group-hover:translate-x-0`}>
+              <div className={`absolute -left-10 -top-10 md:left-full rounded-md px-2 py-1 ml-6 bg-indigo-100 text-indigo-800 text-sm invisible opacity-20 -translate-x-3 transition-all group-hover:visible group-hover:opacity-100 group-hover:translate-x-0`}>
                  {item.text}
               </div>
             )}
