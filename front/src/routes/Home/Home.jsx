a04d091bd38ee7e8a2576cddd403e45bc86ebe1b
Jorge Trujillo <39315478+xiriuxb@users.noreply.github.com>
2024-03-13 11:10:26 -0500

    Update home and fix user link in recipe

--- front/src/routes/Home/Home.jsx
+++ front/src/routes/Home/Home.jsx
@@ -1,35 +1,27 @@
 import { useState, useEffect, useRef } from "react";
-import { FaRegUserCircle } from "react-icons/fa";
-import { TfiCommentAlt } from "react-icons/tfi";
-import { HiOutlineBookmark } from "react-icons/hi2";
-import { useAuthContext } from "../../context/AuthProvider";
-import { Link } from "react-router-dom";
 import appApi from "../../api/appApi";
-import LikesComponent from "../../components/Likes/LikesComponent";
+import useInfiniteScroll from "../../hooks/useInfiniteScroll";
+import RecipeCardComponent from "../../components/RecipeCard/RecipeCardComponent";
 
 const Home = () => {
   const [dishList, setDishList] = useState([]);
-  const [dishAux, setDishAux] = useState([]);
   const [loading, setLoading] = useState(true);
-  const {
-    addOrRemoveFromBookmark,
-    bookMark,
-    user,
-  } = useAuthContext();
-  const { loaderRef, page, setPage, hasMore, setHasMore } = useInfiniteScroll(
+  const scrollRef = useRef(null);
+  const { page, setPage, hasMore, setHasMore } = useInfiniteScroll(
+    scrollRef,
     (newPage) => setPage(newPage + 1)
   );
 
   useEffect(() => {
     const fetchRecipe = async () => {
-      setLoading(true);
       try {
+        setLoading(true);
         const res = await appApi.get(`/recipes?page=${page}`);
         const resApi = res?.data?.recipes;
         if (page === 1) {
-          setDishAux(resApi);
+          setDishList(resApi);
         } else {
-          setDishAux((prevRec) => [...prevRec, ...resApi]);
+          setDishList((prevRec) => [...prevRec, ...resApi]);
         }
         if (resApi.length === 0 || resApi.length < 10) {
           setHasMore(false);
@@ -42,153 +34,32 @@ const Home = () => {
         console.error("Error fetching recipe:", error);
       }
     };
-    if(hasMore){
+    if (hasMore) {
       fetchRecipe();
     }
-  }, [page, hasMore, setHasMore]);
-
-  useEffect(() => {
-    const fetchRecipe = async () => {
-      try {
-        const books = bookMark?.map((bookMark) => bookMark.id);
-
-        const newDataApi = dishAux.map((data) => {
-          const newArrayInBook = books?.find((book) => book === data.id);
-
-          if (newArrayInBook) {
-            return { ...data, bookMark: true };
-          } else {
-            return { ...data, bookMark: false };
-          }
-        });
-        newDataApi.sort((a, b) => new Date(b.date) - new Date(a.date));
-        setDishList(user ? newDataApi : dishAux);
-      } catch (error) {
-        console.error("Error fetching recipe:", error);
-      }
-    };
-
-    fetchRecipe();
-  }, [bookMark, dishAux, user]);
+  }, [page, hasMore]);
 
   return (
     <main className="flex justify-center px-4 mt-5">
       <section className="max-w-[1200px] md:w-full">
         <div className="my-[5%] mx-0">
-          <div className="flex flex-col lg:gap-x-4  md:gap-x-0 lg:gap-y-12 md:gap-y-24 sm:mb-5 justify-center items-center">
+          <div className="flex flex-col lg:gap-x-4  md:gap-x-0 lg:gap-y-1 md:gap-y-1 sm:mb-5 justify-center items-center">
             {dishList.map((val) => (
-              <div
-                className="md:max-w-[550px] md:w-[550px] md:h-[380px] lg:max-w-full lg:w-full lg:h-full gap-4"
-                key={val?.id}
-              >
-                <div className="flex flex-col w-full bg-white border border-solid rounded-xl mb-5 p-5">
-                  <h3 className="flex justify-between items-center pl-2 pb-1">
-                    <Link
-                      className="flex justify-between items-center gap-2 text-l"
-                      to={`/${val.User.user_name}`}
-                    >
-                      <FaRegUserCircle size={20} />
-                      <p className="text-sm font-semibold" id="userPost">
-                        @{val?.User?.user_name}
-                      </p>
-                    </Link>
-                    <p id="date" className="text-sm md:pr-5 sm:p-0">
-                      {new Date(val?.createdAt).toLocaleDateString("es-AR")}
-                    </p>
-                  </h3>
-                  <Link to={`/detail?dishID=${val?.id}`}>
-                    <img
-                      className="pt-2 w-[500px] md:max-h-[230px] lg:w-full lg:max-h-[400px] object-cover rounded-xl"
-                      src={val?.primaryimage}
-                      alt=""
-                    />
-                  </Link>
-                  <div className="flex justify-between items-center py-3">
-                    <div className="flex items-center flex-row">
-                      <LikesComponent recipeId={val.id} />
-                      <button
-                        onClick={addOrRemoveFromBookmark}
-                        data-bookmark-id={val?.id}
-                        className="flex seft-start item-center gap-x-2 pl-2"
-                      >
-                        {val.bookMark ? (
-                          <HiOutlineBookmark
-                            className={`cursor-pointer  fill-red-700 text-red-700 `}
-                            size={20}
-                          />
-                        ) : (
-                          <HiOutlineBookmark
-                            className={`cursor-pointer `}
-                            size={20}
-                          />
-                        )}
-                      </button>
-                    </div>
-                    <div>
-                      <button className="flex justify-center item-center pr-2">
-                        <TfiCommentAlt className="cursor-pointer" size={20} />
-                      </button>
-                    </div>
-                  </div>
-                  <h3 id="name" className="pb-2">
-                    {val?.name}
-                  </h3>
-
-                  <div
-                    id="comentary"
-                    className="border text-justify border-solid rounded-xl p-2 md:h-[full] lg:[full] "
-                  >
-                    {val?.description?.substring(0, 120)}...
-                  </div>
-                </div>
-              </div>
+              <RecipeCardComponent key={val.id} recipeInfo={val} />
             ))}
           </div>
           {hasMore && (
             <>
+              {!loading && <div ref={scrollRef}></div>}
               <div className="mt-12 mb-16 w-full flex justify-center">
                 <span className="loader justify-center"></span>
               </div>
-              {!loading && <div ref={loaderRef}></div>}
             </>
           )}
         </div>
       </section>
     </main>
   );
-};
-
-const useInfiniteScroll = (callback) => {
-  const loaderRef = useRef(null);
-  const [page, setPage] = useState(1);
-  const [hasMore, setHasMore] = useState(true);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting && loaderRef.current) {
-          if (hasMore) {
-            callback(page);
-          }
-        }
-      },
-      {
-        threshold: 0.9,
-      }
-    );
-
-    if (loaderRef.current) {
-      observer.observe(loaderRef.current);
-    }
-
-    return () => {
-      if (loaderRef.current) {
-        observer.unobserve(loaderRef.current);
-      }
-    };
-  }, [callback]);
-
-  return { loaderRef, page, hasMore, setHasMore, setPage };
 };
 
 export default Home;
--- front/src/routes/Recipes/index.jsx
+++ front/src/routes/Recipes/index.jsx
@@ -1,12 +1,9 @@
-import { FaRegUserCircle } from "react-icons/fa";
-import { TfiCommentAlt } from "react-icons/tfi";
-import { HiOutlineBookmark } from "react-icons/hi2";
 import { useAuthContext } from "../../context/AuthProvider";
-import { Navigate, Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import appApi from "../../api/appApi";
 import { BiSolidLike } from "react-icons/bi";
-import LikesComponent from "../../components/Likes/LikesComponent";
+import RecipeCardComponent from "../../components/RecipeCard/RecipeCardComponent";
 
 const Recipes = () => {
   const { logIn } = useAuthContext();
@@ -42,9 +39,9 @@ const Recipes = () => {
           <section className="lg:w-[1200px] h-full">
             {dishList.length ? (
               <div className="my-[5%] mx-0">
-                <div className="flex flex-wrap md:gap-4 md:gap-y-24 justify-center items-center pb-20">
+                <div className="flex flex-wrap md:gap-4 md:gap-y-1 justify-center items-center pb-20">
                   {dishList.map((val) => (
-                    <RecipeDetailsComponent
+                    <RecipeCardComponent
                       key={val.id}
                       recipeInfo={val}
                       callbackToLikesComp={removeFromLikesList}
@@ -64,86 +61,6 @@ const Recipes = () => {
         </main>
       )}
     </>
-  );
-};
-
-const RecipeDetailsComponent = ({ recipeInfo, callbackToLikesComp }) => {
-  const { bookMark, addOrRemoveFromBookmark } = useAuthContext();
-  const [isBookmarked, setIsBookmarked] = useState(false);
-  const books = bookMark?.map((bookMark) => bookMark.id);
-  useEffect(() => {
-    const fetchRecipe = () => {
-      if (books.includes(recipeInfo.id)) {
-        setIsBookmarked(true);
-      } else {
-        setIsBookmarked(false);
-      }
-    };
-
-    fetchRecipe();
-  }, [bookMark]);
-
-  return (
-    <div className="md:max-w-[550px] min-w-[full] md:h-[380px] sm:h-full">
-      <div className="flex flex-col w-full bg-white border border-solid rounded-xl mb-5 p-5">
-        <h3 className="flex justify-between items-center pl-2 pb-1">
-          <span className="flex justify-between items-center gap-2 text-l">
-            <FaRegUserCircle size={20} />
-            <Link to={`/${recipeInfo.User.id}`}>
-              <p className="text-sm font-semibold" id="userPost">
-                @{recipeInfo.User.user_name}
-              </p>
-            </Link>
-          </span>
-          <p id="date" className="text-sm md:pr-5 sm:p-0">
-            {new Date(recipeInfo.createdAt).toLocaleDateString("es-AR")}
-          </p>
-        </h3>
-        <Link to={`/detail?dishID=${recipeInfo.id}`}>
-          <img
-            className="pt-2 w-[500px] max-h-[230px] object-cover rounded-xl"
-            src={recipeInfo.primaryimage}
-            alt=""
-          />
-        </Link>
-        <div className="flex justify-between items-center py-3">
-          <div className="flex flex-row">
-            <LikesComponent
-              recipeId={recipeInfo.id}
-              callbackToLikesComp={callbackToLikesComp}
-            />
-            <button
-              onClick={addOrRemoveFromBookmark}
-              data-bookmark-id={recipeInfo?.id}
-              className="flex seft-start item-center gap-x-2 pl-2"
-            >
-              {isBookmarked ? (
-                <HiOutlineBookmark
-                  className={`cursor-pointer  fill-red-700 text-red-700 `}
-                  size={20}
-                />
-              ) : (
-                <HiOutlineBookmark className={`cursor-pointer `} size={20} />
-              )}
-            </button>
-          </div>
-          <div>
-            <button className="flex justify-center item-center pr-2">
-              <TfiCommentAlt className="cursor-pointer" size={20} />
-            </button>
-          </div>
-        </div>
-        <h3 id="name" className="pb-2">
-          {recipeInfo.name}
-        </h3>
-        <p
-          id="comentary"
-          className="border text-justify border-solid rounded-xl p-2 md:h-20 sm:h-full"
-        >
-          {recipeInfo.description?.substring(0, 120)}...
-        </p>
-      </div>
-    </div>
   );
 };
 
